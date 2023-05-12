37d65fa5b0207e8328b3c3e82ccce7814b10e4de
Jonathan <eaajonhl@students.eaaa.dk>
2023-05-12 12:13:33 +0200

    First commit

---
+++ auctionServiceAPI/Controllers/AuctionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace auctionServiceAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuctionController : ControllerBase
+{
+
+    private readonly ILogger<AuctionController> _logger;
+
+    public AuctionController(ILogger<AuctionController> logger)
+    {
+        _logger = logger;
+    }
+
+}
