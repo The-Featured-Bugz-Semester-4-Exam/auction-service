8881b82b359fe5437423f79e0fd0440285b9df1c
Jacob <jacobvestergaard@hotmail.dk>
2023-05-16 20:19:38 +0200

    Mongodb kommunikation virker og et eksemplar på api vi skal bruge med hardcore i

--- auctionServiceAPI/Controllers/AuctionController.cs
+++ auctionServiceAPI/Controllers/AuctionController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using RabbitMQ.Client;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
 namespace auctionServiceAPI.Controllers;
 
 [ApiController]
@@ -12,6 +17,11 @@ public class AuctionController : ControllerBase
     private string mongodbConnection = string.Empty;
 
     private readonly IMongoDatabase _database;
+
+    private ConnectionFactory factory = new ConnectionFactory();
+    private IConnection connection;
+    private IModel channel;
+
 
     public AuctionController(ILogger<AuctionController> logger,IConfiguration configuration)
     {
@@ -28,49 +38,64 @@ public class AuctionController : ControllerBase
         _logger = logger;
     }
     [HttpPost]
-    public void PostAuction(){
+    public void PostAuction(Item item){
         
-        Item itemm = new Item
-        {
-            ItemID = 1,
-            ItemName = "Item 1",
-            ItemDescription = "Beskrivelse af Item 1",
-            ItemStartprice = 100,
-            ItemSellerID = 123,
-            ItemStartDate = DateTime.Parse("2022-01-01T00:00:00Z"),
-            ItemEndDate = DateTime.Parse("2022-01-10T00:00:00Z")
-        };
-        Auction auction = new Auction(1,itemm,-1);
+        Auction auction = new Auction(99,item,-1);
+
+
 
 
         _database.GetCollection<Auction>("AuctionCollection").InsertOne(auction);
     }
 
-  /* [HttpGet]
-    public List<Auction> GetAuctionsActive(){
-        var collection = _database.GetCollection<Auction>("YourAuctionCollection");
-
-    // Udfør forespørgsel for at hente aktive auktioner baseret på dine kriterier
-    var activeAuctions = collection.Find(a => a.).ToList();
-   
-    return activeAuctions;
-    }*/
-    [HttpGet]
-    public Auction GetAuction(){
+   [HttpGet("GetAuctionsActive")]
+    public List<Auction> GetAllAuctionActive(){
         var collection = _database.GetCollection<Auction>("AuctionCollection");
 
-
-        var filter = Builders<Auction>.Filter.Eq(a => a.AuctionID,1);
-        var projection = Builders<Auction>.Projection
-        .Include(a => a.AuctionID)
-        .Include(a => a.Item)
-        .Include(a => a.AuctionWinnerID);
-
-        Auction auction = collection.Find(filter).Project<Auction>(projection).FirstOrDefault();
-
+    // Udfør forespørgsel for at hente aktive auktioner baseret på dine kriterier
+    var activeAuctions = collection.Find(_ => true).ToList();
    
+    return activeAuctions;
+    }
+
+
+    
+    [HttpGet("GetAuctionActive/{auctionId}")]
+    public Auction GetAuctionActive(int auctionId){
+        var collection = _database.GetCollection<Auction>("AuctionCollection");
+        
+        Auction auction = collection.Find(x => x.AuctionID == auctionId).FirstOrDefault();
         return auction;
     }
- 
+    [HttpPost("PostAuctionBid/{auctionBid}")]
+    public IActionResult PostAuctionBid(AuctionBid auctionBid){
+            try
+        {
+            _logger.LogInformation("WorkshopRequest oprettet" + StatusCodes.Status200OK,    // Logger en information om, at WorkshopRequest er oprettet, med HTTP status 200 OK og tidspunktet.
+            DateTime.UtcNow.ToLongTimeString());
 
+            // Exchange(topic_logs) bestemmer, hvilken type meddelelse der sendes til hvilken kø "Repair" eller "Service".
+            channel.ExchangeDeclare(exchange: "topic_logs", ExchangeType.Topic);
+            string message = JsonConvert.SerializeObject(auctionBid);    // Konverterer WorkshopRequest til en JSON-streng.
+            var body = Encoding.UTF8.GetBytes(message);    // Konverterer JSON-strengen til en byte-array.
+        
+            // ServiceType datatype String skal være "Repair" eller "Service" !!!
+            channel.BasicPublish(exchange: "topic_logs",
+                                 // Sender beskeden til køen, der passer til ServiceType, som kan være "Repair" eller "Service".
+                                 routingKey: "TestWay",
+                                 basicProperties: null,
+                                 body: body);
+
+            _logger.LogInformation($"WorkshopRequest added - {message}");    // Logger en information om, at WorkshopRequest er tilføjet med WorkshopRequest JSON-strengen.
+
+            return Ok(message);    // Returnerer HTTP status 200 OK med WorkshopRequest JSON-strengen i responskroppen.
+        }
+        catch (Exception)    // Håndterer exceptions og logger dem med tidspunktet.
+        {
+            _logger.LogInformation("Fejl, WorkshopRequest ikke oprettet",
+            DateTime.UtcNow.ToLongTimeString());
+
+            return null;
+        }
+    }
 }
---
+++ auctionServiceAPI/Models/AuctionBid.cs
@@ -0,0 +1,13 @@
+public class AuctionBid
+{
+    public int AuctionBidID {get; set;}
+    public string AuctionBidUserID { get; set; } = string.Empty;
+
+    public int Bid { get; set; }    
+    public AuctionBid(int auctionBidID,string auctionBidUserID,int bid)
+    {
+        this.AuctionBidID = auctionBidID;
+        this.AuctionBidUserID = auctionBidUserID;
+        this.Bid = bid;
+    }
+}
