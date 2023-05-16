45ca12f4ae85dfe4ec7264236b81f80ec90d2379
Jacob <jacobvestergaard@hotmail.dk>
2023-05-16 10:21:48 +0200

    Models Again

--- auctionServiceAPI/Models/Auction.cs
+++ auctionServiceAPI/Models/Auction.cs
@@ -1,5 +1,6 @@
-class Auction
+public class Auction
 {
     public int AuctionID { get; set; }
     public Item Item { get; set; }
+    public int AuctionWinnerID {get; set; } 
 }
--- auctionServiceAPI/Models/Item.cs
+++ auctionServiceAPI/Models/Item.cs
@@ -1,4 +1,4 @@
-class Item
+public class Item
 {
     public int ItemID { get; set; }
     public string ItemName { get; set; } = string.Empty;
