2a9f452250840d439ac7390fbe089ffdfe336219
Jacob <jacobvestergaard@hotmail.dk>
2023-05-16 10:50:30 +0200

    mongodbConnection

--- auctionServiceAPI/Controllers/AuctionController.cs
+++ auctionServiceAPI/Controllers/AuctionController.cs
@@ -9,12 +9,18 @@ public class AuctionController : ControllerBase
 
     private readonly ILogger<AuctionController> _logger;
 
+    private string mongodbConnection = string.Empty;
+
     public AuctionController(ILogger<AuctionController> logger,IConfiguration configuration)
     {
+        mongodbConnection = configuration["mogodbConnection"] ?? string.Empty;
         //Hej med dig
         _logger = logger;
     }
-   
-    
+    public void PostAuction(Item item){
+        
+
+      
+    }
 
 }
