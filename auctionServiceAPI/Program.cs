a7630def3d4bd96a79e27b4d36f463d9098782d3
Jacob <jacobvestergaard@hotmail.dk>
2023-05-19 08:08:11 +0200

    ikke møj

--- auctionServiceAPI/Program.cs
+++ auctionServiceAPI/Program.cs
@@ -1,34 +1,56 @@
-using System.Timers;
-using auctionServiceAPI.Controllers;
 using auctionServiceAPI.Models;
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
 
 
-
-
-
-var timerTick = new TimerTick();
-timerTick.Start();
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    var logger =
+    NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+    logger.Debug("init main");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+ //   var timerTick = new TimerTick();
+  //  timerTick.Start();
+
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+
+
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+
+    var app = builder.Build();
+
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+    
+}
+catch (System.Exception ex )
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+    
+}
+finally 
+{
+    NLog.LogManager.Shutdown();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
